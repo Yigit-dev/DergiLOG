800995d11126efef787b78c9cd29bb3c616dacd3
= <=>
2023-02-13 23:59:35 +0300

    Setup Frontend Environment

---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="bg-red-400 p-4">
+      <h2 class="font-bold text-green-400">Card Title</h2>
+      <p class="text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, obcaecati. Dicta sequi provident officia minus
+        itaque. Provident sequi, voluptate eius debitis maiores quas dolorem, reprehenderit et alias adipisci porro
+        amet!
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Index',
+}
+</script>
