9beb6e453421851749f90503a5dc99b38a90635c
= <=>
2023-02-14 21:29:47 +0300

    App js Error handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+require('express-async-errors')
 const express = require('express')
 require('dotenv').config()
 require('./config/databaseConnection.js')
@@ -7,15 +8,15 @@ const cors = require('cors')
 const port = process.env.PORT || 500
 const indexRouter = require('./routes/index')
 const cookieParser = require('cookie-parser')
-
+const { errorHandlerMiddleware } = require('./middlewares/error.handler')
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 app.use(cookieParser())
 app.use(indexRouter)
+app.use(errorHandlerMiddleware)
 
 app.listen(port, () => {
   console.log(`server listening on port : ${port}`)
