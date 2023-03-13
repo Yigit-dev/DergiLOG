ba55add2bf0089bf5c2c7d60fcae18d409025d2d
Kubilay <kubikarabag@gmail.com>
2023-03-13 21:18:32 +0300

    Bugfixes

--- backend/src/models/Admin.js
+++ backend/src/models/Admin.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 const adminSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-  journal_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Journal' },
+  company_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
 })
 module.exports = new mongoose.model('Admin', adminSchema)
