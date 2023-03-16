5799569f7d9b0b68a994f2c3ad967aed12a2f5b1
Kubilay <kubikarabag@gmail.com>
2023-03-16 21:20:30 +0300

    get company fixed

--- frontend/stores/company.store.js
+++ frontend/stores/company.store.js
@@ -17,15 +17,17 @@ export const useCompanyStore = defineStore('CompanyStore', {
             'x-api-key': useRuntimeConfig().API_KEY,
           },
         }).then(res => {
+          console.log(res.data.company_info)
           this.company = res.data.company_info
         })
       })
       await $fetch(`http://localhost:3000/company/${this.company.company_name}/journals`, {
         method: 'GET',
         headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDAzOTdkYTRmMDhkOTliYTFlMzk1ZGMiLCJuYW1lIjoiS3ViaWxheSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY3ODk3Nzc5MiwiZXhwIjoxNjc5MDY0MTkyfQ.qi5VfQtZphLaMcrIQBtmVXLgj_2O6_61oFmo7GdpxSU`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDAzOTdkYTRmMDhkOTliYTFlMzk1ZGMiLCJuYW1lIjoiS3ViaWxheSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY3ODk5MDcyNCwiZXhwIjoxNjc4OTkxNjI0fQ.8a9LcUy7qD_FBAYAFdtLO3lD1g4vmcbkEUbYHS64_mg`,
           'x-api-key': useRuntimeConfig().API_KEY,
         },
+        credentials: 'include',
       }).then(res => console.log(res))
     },
   },
